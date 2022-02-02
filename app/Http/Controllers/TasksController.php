75864b0640583f380789d265284ac359e7ffc31d
Mukotso <kelvinmukotso@gmail.com>
2022-02-02 08:49:56 +0300

    laravel getting started with repo and custom middlewware

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Task;
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
+
+
+class TaskController extends Controller
+{
+
+    protected $tasks;
+    //injecting in TaskREpository
+    public function __construct(TaskRepository $tasks)
+    {
+        $this->middleware('auth');
+
+        $this->tasks = $tasks;
+    }
+
+    public function index(Request $request)
+    {
+        return $this->tasks->getUserTasks($request->user());
+    }
+}
